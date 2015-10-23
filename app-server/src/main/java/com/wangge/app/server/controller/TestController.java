9881e726a72d410380a141750b064a39870b5d65
yinxunguo <yinxunguo@yinxunguo>
2015-10-23 15:47:03 +0800

    测试接口

--- app-server/src/main/java/com/wangge/app/server/controller/TestController.java
+++ app-server/src/main/java/com/wangge/app/server/controller/TestController.java
@@ -1,5 +1,24 @@
 package com.wangge.app.server.controller;
 
-public class TestController {
+import java.util.List;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@Autowired
+	
+	@RequestMapping(value = "/test")
+	public ResponseEntity<String> test(String test) {
+		System.out.println("test:"+test);
+		JSONObject obj=new JSONObject("{\"name\":\"xiaoming\"}");
+		System.out.println(new ResponseEntity<JSONObject>(obj, HttpStatus.OK));
+		String jsonStr="{\"name\":\"xiaoming\"}";
+		return new ResponseEntity<String>(jsonStr, HttpStatus.OK);
+	}
 }
