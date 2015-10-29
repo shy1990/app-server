1e476bfbd55479275660d32a1688e0d06d49c24b
changjun <changjun@changjun>
2015-10-29 18:43:14 +0800

    jpush

---
+++ app-server/src/main/java/com/wangge/app/server/entity/Message.java
@@ -0,0 +1,66 @@
+package com.wangge.app.server.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+@Entity
+@Table(schema = "SANJI", name = "T_MESSAGE")
+public class Message extends AbstractPersistable<Long>{
+	
+	private static final long serialVersionUID = 1L;
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+	private String content;
+	private String receiver;
+	private String result;
+	private String type;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
