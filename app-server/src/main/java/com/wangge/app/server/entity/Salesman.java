066857e79ab6d73cd599f46a8f8388c6c55e1d2d
changjun <changjun@changjun>
2015-10-30 15:04:22 +0800

    cj

--- app-server/src/main/java/com/wangge/app/server/entity/Salesman.java
+++ app-server/src/main/java/com/wangge/app/server/entity/Salesman.java
@@ -1,24 +1,42 @@
 package com.wangge.app.server.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.wangge.common.entity.Region;
 import com.wangge.common.entity.User;
 
 /**
  * 业务员
+ * 
  * @author wujiming
  *
  */
 @Entity
 @Table(schema = "SANJI", name = "T_SALESMAN")
-public class Salesman extends User{
-	
-	/**
-	 * 
-	 */
+public class Salesman extends User {
 	private static final long serialVersionUID = 1L;
+	@OneToOne
+	private Region region;
 	private String phone;
+
+	public Salesman() {
+		super();
+	}
+
+	public Salesman(String id, String username, String password, Region region) {
+		super(id, username, password);
+		this.region = region;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
 
 	public String getPhone() {
 		return phone;
@@ -27,6 +45,5 @@ public class Salesman extends User{
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
+
 }
