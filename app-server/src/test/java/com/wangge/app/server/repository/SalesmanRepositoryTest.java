8ee7ed6fe84e43cfe71c4f74a81f7cf7de1d80b7
盛堡镇 <Administrator@sjk001-PC>
2015-11-03 14:33:54 +0800

    aa

--- app-server/src/test/java/com/wangge/app/server/repository/SalesmanRepositoryTest.java
+++ app-server/src/test/java/com/wangge/app/server/repository/SalesmanRepositoryTest.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.wangge.AppServerApplication;
 import com.wangge.app.server.entity.Salesman;
+import com.wangge.app.server.entity.TaskSaojie;
+import com.wangge.common.entity.Region;
+import com.wangge.common.entity.User;
 import com.wangge.common.repository.RegionRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,10 +22,36 @@ public class SalesmanRepositoryTest {
 	private SalesmanRepository sr;
 	@Autowired
 	private RegionRepository rr;
-	@Test
+	@Autowired
+	private SaojieDateRepository saojieDateRepository;
+	
+	/*@Test
 	public void testAdd() {
 		Salesman entity=new Salesman("2", "yewu01", "yewu01", rr.findOne("101"));
 		sr.save(entity);
+	}*/
+	@Test
+	public  void testSales(){
+		
+		//Salesman u = new Salesman();
+		
+		
+		Salesman wu = sr.findByUsernameAndPasswordAndPhone("yewuzhangsan","yewuzhangsan","phone");
+		System.out.println("============="+ wu.getUsername());
 	}
 
+	@Test
+	public  void test2(){
+		//saojieDateRepository.findByRegionId("");
+		Salesman s = sr.findByUsername("yewuzhangsan");
+		for(TaskSaojie tsj : s.getTaskSaojie()){
+			System.out.println("========"+tsj.getTaskSaoJieRegion());
+			
+		}
+		System.out.println("==========="+s.getTaskSaojie());
+		
+		
+	}
+	
+	
 }
