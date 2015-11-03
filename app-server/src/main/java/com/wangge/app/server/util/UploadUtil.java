115e1662973ae79513bffd2a9f15af3402a8b1ca
盛堡镇 <Administrator@sjk001-PC>
2015-11-03 14:49:03 +0800

    扫街任务，扫街区域实体been

---
+++ app-server/src/main/java/com/wangge/app/server/util/UploadUtil.java
@@ -0,0 +1,36 @@
+package com.wangge.app.server.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class UploadUtil {
+	
+	
+	 public  static String saveImg(File file , String filePath,String fileName) throws Exception{
+		 
+		 InputStream is = new FileInputStream(file);  
+         File filedir = new File(filePath);    
+         if (!filedir.exists())    
+             filedir.mkdirs();
+         
+         OutputStream os = new FileOutputStream(new File(filedir,fileName));  
+         byte[] byteStr = new byte[1024];    
+         int len = 0;    
+         while ((len = is.read(byteStr)) > 0) {    
+             os.write(byteStr,0,len);    
+         }    
+         is.close();    
+             
+         os.flush();    
+         os.close();    
+		 
+		return filePath+fileName;
+ 	
+ }
+	 
+}
