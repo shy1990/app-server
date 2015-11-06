98dc02a85196eb2615013989da4e958e6c2d705c
盛堡镇 <Administrator@sjk001-PC>
2015-11-06 14:09:33 +0800

    as

--- app-server/src/main/java/com/wangge/app/server/entity/Region.java
+++ app-server/src/main/java/com/wangge/app/server/entity/Region.java
@@ -1,23 +1,19 @@
 package com.wangge.app.server.entity;
 
 import java.io.Serializable;
-import java.util.Collection;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -37,17 +33,8 @@ public abstract class Region implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="parent_id")
 	private Region parent;
-	@ManyToMany(mappedBy="regions",cascade=CascadeType.REFRESH,fetch = FetchType.EAGER)
-	private Collection<Task> task;
 	
 
-	public Collection<Task> getTask() {
-		return task;
-	}
-
-	public void setTask(Collection<Task> task) {
-		this.task = task;
-	}
 
 	public String getId() {
 		return id;
--- app-server/src/main/java/com/wangge/app/server/service/DataSaojieService.java
+++ app-server/src/main/java/com/wangge/app/server/service/DataSaojieService.java
@@ -5,13 +5,10 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.wangge.app.server.entity.DataSaojie;
 import com.wangge.app.server.entity.Region;
-import com.wangge.app.server.entity.Task;
-import com.wangge.app.server.entity.TaskSaojie;
 import com.wangge.app.server.repository.DataSaojieRepository;
 import com.wangge.app.server.repository.RegionRepository;
 import com.wangge.app.server.repository.TaskSaojieRepository;
@@ -20,32 +17,22 @@ import com.wangge.app.server.repository.TaskSaojieRepository;
 public class DataSaojieService {
 	@Resource
 	private DataSaojieRepository dataSaojieRepository;
-	
+
 	@Autowired
 	private TaskSaojieRepository taskSaojieRepository;
-	
+
 	@Autowired
 	private RegionRepository regionRepository;
-	
-	public void addDataSaojie(DataSaojie dataSaojie){
-		
+
+	public void addDataSaojie(DataSaojie dataSaojie) {
+
 		dataSaojieRepository.save(dataSaojie);
 	}
-	
-	
-	public List<DataSaojie> getSaojieDataByregionId(String regionId){
-		
-		  Region region =  regionRepository.findById(regionId);
-		  
-		  for(Task task : region.getTask()){
-			  task.getDataSaojie();
-			  return (List<DataSaojie>)  task.getDataSaojie();
-		  }
-		   			
-	//		TaskSaojie s = taskSaojieRepository.findByRegions(regionId);
-			
-			//return (List<DataSaojie>) s.getDataSaojie();
-		  return null;
+
+	public List<DataSaojie> getSaojieDataByregion(Region region) {
+
+
+		return dataSaojieRepository.findByTaskRegionsIn(region);
 	}
 
 }
