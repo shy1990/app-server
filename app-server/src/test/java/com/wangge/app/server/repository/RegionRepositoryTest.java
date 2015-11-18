a5bf6596b5033ad94fc7d4a364e3d779dcca18ed
wujiming <wzslw@163.com>
2015-11-18 17:32:12 +0800

    升级

--- app-server/src/test/java/com/wangge/app/server/repository/RegionRepositoryTest.java
+++ app-server/src/test/java/com/wangge/app/server/repository/RegionRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.wangge.app.server.repository;
 
-import java.util.List;
+import java.util.Collection;
+
+import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +11,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.wangge.AppServerApplication;
-import com.wangge.app.server.entity.CustomRegion;
-import com.wangge.app.server.entity.Region;
+import com.wangge.common.entity.Region;
+import com.wangge.common.repository.RegionRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AppServerApplication.class)
@@ -20,15 +22,22 @@ public class RegionRepositoryTest {
 
 	@Test
 	public void testAdd() {
-		Region entity=new CustomRegion("3","逗逼国", "125.681496-42.913345=125.67182-42.90182=125.66952-42.885338=125.67182-42.869267=125.681496-42.854794=125.70459-42.85108=125.72654-42.854794=125.74253-42.864193=125.74368-42.88322=125.73966-42.89929=125.72654-42.913345=125.70689-42.922108=");
+		Region entity = new Region("100", "钓鱼岛",Region.RegionType.CITY);
+		entity.setCoordinates(
+				"125.681496-42.913345=125.67182-42.90182=125.66952-42.885338=125.67182-42.869267=125.681496-42.854794=125.70459-42.85108=125.72654-42.854794=125.74253-42.864193=125.74368-42.88322=125.73966-42.89929=125.72654-42.913345=125.70689-42.922108=");
 		entity.setParent(rr.getOne("0"));
 		rr.save(entity);
 	}
-	
+
 	@Test
-	public void testFind(){
-		List<Region> regionList=(List<Region>) rr.findOne("370100").getRegions();
-		System.out.println(regionList);
+	@Transactional
+	public void testFind() {
+		Region entity = rr.findOne("0");
+		System.out.println(entity.getName()+ entity.getType().getName());
+		Collection<Region> children = entity.getChildren();
+		for (Region region : children) {
+			System.out.println(region.getName()+region.getType().getName());
+		}
 	}
 
 }
