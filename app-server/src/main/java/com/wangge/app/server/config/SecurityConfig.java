c7e48803fdba3f0b9cb023f0411f4db5bcb4899c
wujiming <wzslw@163.com>
2015-11-18 18:15:04 +0800

    修改认证方式

--- app-server/src/main/java/com/wangge/app/server/config/SecurityConfig.java
+++ app-server/src/main/java/com/wangge/app/server/config/SecurityConfig.java
@@ -6,26 +6,24 @@ import java.util.Map;
 
 import javax.servlet.Filter;
 
-import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.authc.AnonymousFilter;
 import org.apache.shiro.web.filter.session.NoSessionCreationFilter;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 
-import com.wangge.app.server.shiro.realm.SalesmanHmacReaml;
 import com.wangge.security.shiro.web.filter.authc.HmacAuthcFilter;
 
 @Configuration
 @Profile("prod")
-public class ShiroConfig {
-
+public class SecurityConfig {
 
 	@Bean(name = "shiroFilter")
-	public ShiroFilterFactoryBean shiroFilter() {
+	@DependsOn("securityManager")
+	public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
 		ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
 		shiroFilter.setLoginUrl("/login");
 		shiroFilter.setSuccessUrl("/");
@@ -38,29 +36,12 @@ public class ShiroConfig {
 		shiroFilter.setFilters(filters);
 
 		Map<String, String> filterChainDefinitionMapping = new LinkedHashMap<String, String>();
-		
+
 		filterChainDefinitionMapping.put("/login", "anon");
 		filterChainDefinitionMapping.put("/**", "rest,hmacAuthc");
 		shiroFilter.setFilterChainDefinitionMap(filterChainDefinitionMapping);
-		shiroFilter.setSecurityManager(securityManager());
+		shiroFilter.setSecurityManager(securityManager);
 		return shiroFilter;
-	}
-
-	@Bean(name = "securityManager")
-	public org.apache.shiro.mgt.SecurityManager securityManager() {
-		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-		securityManager.setRealm(salesmanRealm());
-		return securityManager;
-	}
-	
-	@Bean
-	@DependsOn({ "lifecycleBeanPostProcessor", "salesmanRepository" })
-	public SalesmanHmacReaml salesmanRealm() {
-		return new SalesmanHmacReaml();
-	}
-	@Bean
-	public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-		return new LifecycleBeanPostProcessor();
 	}
 
 }
