335b27f325bf902b7bfa3601e32519681aa5d2be
wujiming <wzslw@163.com>
2015-11-20 17:55:57 +0800

    根据区域查询订单

--- app-server/src/main/java/com/wangge/app/server/repository/OrderRepository.java
+++ app-server/src/main/java/com/wangge/app/server/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.wangge.app.server.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wangge.app.server.entity.Order;
+import com.wangge.common.entity.Region;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
+	Page<Order> findByRegion(Region region,Pageable page);
 }
