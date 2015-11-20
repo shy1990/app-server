91a1c5a82bf8a20fa7768860e23cc469a2b238e8
jiabin <954691592@qq.com>
2015-11-20 17:41:43 +0800

    111

--- app-server/src/main/java/com/wangge/app/server/controller/SalesmanController.java
+++ app-server/src/main/java/com/wangge/app/server/controller/SalesmanController.java
@@ -1,20 +1,16 @@
 package com.wangge.app.server.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
-import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
 import com.wangge.app.server.entity.Salesman;
-import com.wangge.app.server.pojo.Json;
-import com.wangge.app.server.repository.SalesmanRepository;
 import com.wangge.app.server.service.SalesmanService;
 
 @RestController
@@ -24,5 +20,11 @@ public class SalesmanController {
 	@Resource
 	private SalesmanService salesmanService;
 
+	@RequestMapping(value = "/findSalesman", method = RequestMethod.POST)
+	public ResponseEntity<List<Salesman>> findSalesman() {
+		List<Salesman> listSalesman=salesmanService.findSalesman();
+			
+	 return new ResponseEntity<List<Salesman>>(listSalesman,HttpStatus.OK);
 	
+}
 }
--- app-server/src/main/java/com/wangge/app/server/service/SalesmanService.java
+++ app-server/src/main/java/com/wangge/app/server/service/SalesmanService.java
@@ -13,43 +13,12 @@ import com.wangge.app.server.repository.SalesmanRepository;
  * @author Administrator
  *
  */
-//@Service
+@Service
 public class SalesmanService {
 
 
 	@Resource
 	private SalesmanRepository salesmanRepository;
 
-	public Salesman findByUsernameAndPassword(String username, String password) {
-
-		Salesman salesman = salesmanRepository.findByUsernameAndPassword(
-				username, password);
-
-		return salesman;
-	}
-
-	public Salesman findByUsername(String username) {
-
-		return salesmanRepository.findByUserUsername(username);
-	}
-
-	public void save(Salesman salesman) {
-		salesmanRepository.save(salesman);
-	}
-
-	public Json login(String username, String password, String phone) {
-		Json json = new Json();
-		Salesman salesman = salesmanRepository.findByUsernameAndPassword(username, password);
-		if(salesman !=null && !"".equals(salesman)){
-			if (!password.equals(salesman.getPassword()) || phone.equals(salesman.getPhone())) {
-				json.setMsg("用戶名或密码错误！");
-			}else if(!phone.equals(salesman.getPhone())){
-				json.setMsg("手机号错误！");
-			}else{
-				json.setMsg("登陆成功！");
-			}
-		}
-		return json;
-	}
-
+	
 }
