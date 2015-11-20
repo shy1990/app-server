9c4f3b7c19d1154eca7cb638f990af3cfc880175
yinxunguo <yinxunguo@yinxunguo>
2015-11-20 18:03:01 +0800

    1

--- app-server/src/main/java/com/wangge/app/server/controller/SalesmanController.java
+++ app-server/src/main/java/com/wangge/app/server/controller/SalesmanController.java
@@ -1,28 +1,33 @@
 package com.wangge.app.server.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
-import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
 import com.wangge.app.server.entity.Salesman;
 import com.wangge.app.server.pojo.Json;
 import com.wangge.app.server.repository.SalesmanRepository;
 import com.wangge.app.server.service.SalesmanService;
+import com.wangge.common.repository.RegionRepository;
 
-//@RestController
+@RestController
 @RequestMapping(value = "/v1/saleman")
 public class SalesmanController {
 
 	@Resource
 	private SalesmanService salesmanService;
+	@Resource
+	private SalesmanRepository smRepository;
 
 	/*@RequestMapping(value = "/{username}/password", method = RequestMethod.PUT)
 	public ResponseEntity<Json> changePassword(@PathVariable("username") String username,
@@ -50,22 +55,25 @@ public class SalesmanController {
 	 */
 	@RequestMapping(value = "/saleList",method = RequestMethod.POST)
 	public ResponseEntity<Json> saleList(){
-		
+		Json json = new Json();
 		List<Salesman> salelist = smRepository.findAll();
 		
 		List<Map<String,Object>> slm = new ArrayList<Map<String,Object>>();
 		
-		for(Salesman salesman : salelist){
-			Map<String,Object> m = new HashMap<String,Object>();
-			m.put("username", salesman.getUsername());
-			m.put("password", salesman.getPassword());
-			m.put("phone", salesman.getPhone());
-			m.put("regionname", salesman.getRegion().getName());
-			m.put("userid", salesman.getId());
-			m.put("regionid", salesman.getRegion().getId());
-			slm.add(m);
+		if(slm != null && slm.size()>0){
+			for(Salesman salesman : salelist){
+				Map<String,Object> m = new HashMap<String,Object>();
+				m.put("username", salesman.getUser().getUsername());
+				m.put("password", salesman.getUser().getPassword());
+				m.put("phone", salesman.getUser().getPhone());
+				m.put("regionname", salesman.getRegion().getName());
+				m.put("userid", salesman.getId());
+				m.put("regionid", salesman.getRegion().getId());
+				slm.add(m);
+			}
 		}
-		
+		json.setObj(slm);
+		return new ResponseEntity<Json>(json,HttpStatus.OK);
 		/*StringBuffer jsbuf=new StringBuffer();
 		int i=0;
 		jsbuf.append("{").append("\"").append("data").append("\"").append(":").append("[");
@@ -80,10 +88,7 @@ public class SalesmanController {
 			.append("\"").append("regionid").append("\"").append(":").append("\"").append(ssm.getRegion().getId()).append("\"}");
 		}
 	jsbuf.append("]}");*/
-		Json j = new Json();
-		j.setObj(slm);
 		
-		return new ResponseEntity<Json>(j,HttpStatus.OK);
 	}
 	
 }
