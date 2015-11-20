38c1d3d7a24346b853bd440bd4f388b6de6cde9e
jiabin <954691592@qq.com>
2015-11-20 11:24:11 +0800

    1

--- app-server/src/main/java/com/wangge/app/server/controller/RegionController.java
+++ app-server/src/main/java/com/wangge/app/server/controller/RegionController.java
@@ -24,7 +24,7 @@ import com.wangge.common.entity.Region;
  * @author SongBaozhen
  *
  */
-//@RestController
+@RestController
 @RequestMapping(value = "/v1")
 public class RegionController {
 
@@ -32,18 +32,6 @@ public class RegionController {
 			.getLogger(RegionController.class);
 	@Resource
 	private RegionService regionService;
-	/**
-	 * 业务人员区域信息
-	 * @param username
-	 * @return
-	 */
-	@RequestMapping(value="/{username}/regions",method=RequestMethod.GET)
-	public ResponseEntity<Map<String,List<Region>>> salesmanRegions(@PathVariable("username") Salesman salesman){
-		logger.debug("username:"+salesman);
-	     Map<String, List<Region>>   regionMap = regionService.getSaojie(salesman);
-	   
-		return new ResponseEntity<Map<String,List<Region>>>(regionMap,HttpStatus.OK);
-	}
 	
 	/**
 	 * 
@@ -61,63 +49,6 @@ public class RegionController {
 	     listTreeVo=regionService.findTreeRegion(id);
 	     System.out.println(new ResponseEntity<List<TreeVo>>(listTreeVo,HttpStatus.OK));
 		return new ResponseEntity<List<TreeVo>>(listTreeVo,HttpStatus.OK);
-	}
-	
-	/**
-	 * 
-	 * 功能: 添加自定义区域
-	 * 详细： 	
-	 * 作者： 	jiabin
-	 * 版本：  1.0
-	 * 日期：  2015年11月6日下午3:54:51
-	 *
-	 */
-	@RequestMapping(value="/saveRegions",method=RequestMethod.POST)
-	public ResponseEntity<String> addPoints(String parentid,String pointStr,String name){
-		logger.debug("parentid"+parentid+"pointStr"+pointStr);
-		/*try {
-			name=Base64.decodeToString(name);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		List<Object> listRegion=regionService.findRegionSort(parentid);
-		int id;
-		if(listRegion.size()>0){
-			Object[]   region   =   (Object[])  listRegion.get(0);
-			
-			id=Integer.parseInt(region[0].toString())+1 ;
-		}else{
-			id=Integer.parseInt(parentid+"00")+1;
-		}
-		Region entity=new Region(String.valueOf(id), name, Region.RegionType.TOWN);
-		entity.setParent(regionService.findRegion(parentid));
-		regionService.saveRegion(entity);
-		return new ResponseEntity<String>("SUCCESS",HttpStatus.OK);
-	}
-	
-	
-	/**
-	 * 
-	 * 功能: dit
-	 * 详细： 	
-	 * 作者： 	jiabin
-	 * 版本：  1.0
-	 * 日期：  2015年11月9日上午9:28:18
-	 *
-	 */
-	@RequestMapping(value="/findbyParentid",method=RequestMethod.POST)
-	public ResponseEntity<List<Region>> findbyParentid(String parentid){
-		logger.debug("parentid"+parentid);
-		/*try {
-			name=Base64.decodeToString(name);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		List<Region> listRegion=regionService.findCustomRegiond(parentid);
-		
-		return new ResponseEntity<List<Region>>(listRegion,HttpStatus.OK);
 	}
 	
 	
--- app-server/src/main/java/com/wangge/app/server/service/RegionService.java
+++ app-server/src/main/java/com/wangge/app/server/service/RegionService.java
@@ -2,49 +2,22 @@ package com.wangge.app.server.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.wangge.app.server.entity.Salesman;
-import com.wangge.app.server.entity.Saojie;
 import com.wangge.app.server.repository.SaojieRepository;
 import com.wangge.app.server.vo.TreeVo;
 import com.wangge.common.entity.Region;
 import com.wangge.common.repository.RegionRepository;
 
-//@Service
+@Service
 public class RegionService {
 	@Autowired
 	private SaojieRepository taskSaojieRepository;
 	@Autowired
 	private RegionRepository regionRepository;
 
-	/**
-	 * 获取扫街区域信息
-	 * 
-	 * @param salesman
-	 * @return Map<String, List<Region>> ,其中 key 为 can,cant.
-	 */
-	public Map<String, List<Region>> getSaojie(Salesman salesman) {
-		Map<String, List<Region>> result = Maps.newHashMap();
-		List<Region> can = Lists.newArrayList();
-		List<Region> cant = Lists.newArrayList();
-		List<Saojie> saojieTasks = taskSaojieRepository.findBySalesman(salesman);
-		// TODO 可用java8 stream过滤
-		for (Saojie taskSaojie : saojieTasks) {
-			if (TaskStatus.PENDING.equals(taskSaojie.getStatus())) {
-				can.addAll(taskSaojie.getRegions());
-			} else {
-				cant.addAll(taskSaojie.getRegions());
-			}
-		}
-		result.put("can", can);
-		result.put("cant", cant);
-		return result;
-	}
 
 	/**
 	 * 
@@ -69,31 +42,4 @@ public class RegionService {
 		return listTreeVo;
 	}
 
-	/**
-	 * 
-	 * 功能: 根据父id查询，用在添加自定义区域id增长 详细： 作者： jiabin 版本： 1.0 日期： 2015年11月6日下午3:50:54
-	 *
-	 */
-	public List<Object> findRegionSort(String id) {
-		List<Object> listRegion = (List<Object>) regionRepository.findByParentId(id);
-		return listRegion;
-	}
-
-	public List<Region> findCustomRegiond(String id) {
-		List<Region> listCustomRegion = (List<Region>) regionRepository.findCustomRegiond(id);
-		return listCustomRegion;
-	}
-
-	/**
-	 * 
-	 * 功能: 查询 详细： 作者： jiabin 版本： 1.0 日期： 2015年11月6日下午5:31:22
-	 *
-	 */
-	public Region findRegion(String id) {
-		return regionRepository.findOne(id);
-	}
-
-	public void saveRegion(Region region) {
-		regionRepository.save(region);
-	}
 }
