7b48d2c44c33071475f3b305950fdf0348ed28f8
yinxunguo <yinxunguo@yinxunguo>
2015-11-20 20:10:43 +0800

    1

--- app-server/src/main/java/com/wangge/app/server/repository/SalesmanRepository.java
+++ app-server/src/main/java/com/wangge/app/server/repository/SalesmanRepository.java
@@ -2,6 +2,7 @@ package com.wangge.app.server.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.wangge.app.server.entity.Salesman;
 
@@ -11,4 +12,7 @@ public interface SalesmanRepository extends JpaRepository<Salesman, String>  {
 //	public Salesman findByUsernameAndPassword(String username,String password);
 //	
 //	public Salesman findByUsernameAndPasswordAndPhone(String username,String password,String phone);
+	
+	@Query("select count(*) from Salesman r where r.region.id=?")
+	public int findSaleNum(String regionid);
 }
