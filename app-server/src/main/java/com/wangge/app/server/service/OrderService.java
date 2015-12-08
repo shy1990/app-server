8ab39c3e501a4ec9df10729a472a2467400f0418
Administrator <Administrator@8U287MAKZ8QZHFS>
2015-12-08 14:42:08 +0800

    1

--- app-server/src/main/java/com/wangge/app/server/service/OrderService.java
+++ app-server/src/main/java/com/wangge/app/server/service/OrderService.java
@@ -3,6 +3,7 @@ package com.wangge.app.server.service;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,36 +13,38 @@ import com.wangge.app.server.entity.Order;
 import com.wangge.app.server.repository.OrderRepository;
 import com.wangge.app.server.vo.OrderPub;
 import com.wangge.common.entity.Region;
+
 @Service
 public class OrderService {
 	@Autowired
 	private OrderRepository or;
-	
+
 	/**
 	 * 
 	 * @Description: 根据地区编码查询所属下的订单
 	 * @param @param region
 	 * @param @param page
-	 * @param @return   
-	 * @return Page<Order>  
+	 * @param @return
+	 * @return Page<Order>
 	 * @throws
 	 * @author changjun
 	 * @date 2015年11月20日
 	 */
-	public List<OrderPub> findByRegion(Region region,Pageable page){
-		Page<Order> pOrder=or.findByRegion(region, page);
-		int totalPage = (pOrder.getTotalPages()+pOrder.getSize()-1)/pOrder.getSize();
+	public List<OrderPub> findByRegion(Region region, Pageable page) {
+		Page<Order> pOrder = or.findByRegion(region, page);
+		int totalPage = (pOrder.getTotalPages() + pOrder.getSize() - 1)
+				/ pOrder.getSize();
 		List<OrderPub> list = new ArrayList<OrderPub>();
-		OrderPub order;//20151105180158329
-		if(pOrder!=null && pOrder.getTotalPages()>0){
-			for(Order o:pOrder){
+		OrderPub order;// 20151105180158329
+		if (pOrder != null && pOrder.getTotalPages() > 0) {
+			for (Order o : pOrder) {
 				order = new OrderPub();
 				order.setTotalPage(totalPage);
 				order.setCreateTime(o.getCreateTime());
 				order.setOrderNum(o.getId());
 				order.setStatus(o.getStatus().getName());
 				order.setUsername(o.getShopName());
-				order.setTotalCost(Double.parseDouble(o.getAmount()+""));
+				order.setTotalCost(Double.parseDouble(o.getAmount() + ""));
 				list.add(order);
 			}
 		}
@@ -51,5 +54,5 @@ public class OrderService {
 	public Order findOne(String ordernum) {
 		return or.findOne(ordernum);
 	}
-	
-}
+
+	}
