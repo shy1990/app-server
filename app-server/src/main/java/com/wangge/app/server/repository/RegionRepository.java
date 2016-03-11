e4041d36a274a972292fdd2f36f8ef3a0925f760
jiabin <954691592@qq.com>
2016-03-11 16:55:54 +0800

    123232

---
+++ app-server/src/main/java/com/wangge/app/server/repository/RegionRepository.java
@@ -0,0 +1,17 @@
+package com.wangge.app.server.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wangge.app.server.entity.Region;
+
+public interface RegionRepository extends JpaRepository<Region, String> {
+	public Region findById(String regionId);
+	
+	public Region findByNameLike(String regionName);
+	
+	public List<Region> findByParentId(String id);
+	
+}
