207fcc23f0ceb68646b7015d4787391231020912
盛堡镇 <Administrator@sjk001-PC>
2016-03-17 17:06:56 +0800

    定时检验未收款报备

---
+++ app-server/src/main/java/com/wangge/appserver/scheduledtasks/UnpaymentRemarkScheduledTask.java
@@ -0,0 +1,47 @@
+package com.wangge.appserver.scheduledtasks;
+
+
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.wangge.app.server.entity.UnpaymentRemark;
+import com.wangge.app.server.repositoryimpl.OrderImpl;
+import com.wangge.app.server.service.UnpaymentRemarkService;
+
+@Component
+@EnableScheduling
+public class UnpaymentRemarkScheduledTask {
+  @Resource
+  private OrderImpl orderImpl;
+  @Resource
+  private UnpaymentRemarkService urs;
+  
+  
+  @Scheduled(cron = "0 0 15 ? * *")
+  public void exsitUnpaymentRemarkStatus(){
+     List<UnpaymentRemark> remarkList = urs.findByCreateTime(new Date());
+     if(remarkList != null && remarkList.size() > 0){
+       for(UnpaymentRemark u : remarkList){
+         Map<String,String> map = orderImpl.findOrderPayStatusByOrderNum(u.getOrderno());
+         if("1".equals(map.get("payStatus"))){
+           u.setStatus(1);
+          
+         }else{
+           u.setStatus(2);
+         }
+         urs.saveUnpaymentRemark(u);
+       }
+     
+     }
+     
+  }
+
+}
