8fc3f992c70f34bedb33cd4854ff3dcbc88170a6
jiabin <954691592@qq.com>
2016-04-14 11:02:10 +0800

    1111

--- app-server/src/main/java/com/wangge/app/server/entity/Message.java
+++ app-server/src/main/java/com/wangge/app/server/entity/Message.java
@@ -37,7 +37,7 @@ public class Message implements Serializable {
 	}
 
 	public enum MessageType {
-		ORDER("下单通知"), SYSTEM("提交审核"), ACTIVE("活动"),CANCELORDER("取消订单通知");
+		ORDER("下单通知"), SYSTEM("提交审核"), ACTIVE("活动"),CANCELORDER("取消订单通知"),SHOUHOU("售后");
 		private String name;
 
 		private MessageType(String name) {
