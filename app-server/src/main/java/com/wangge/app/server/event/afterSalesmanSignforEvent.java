090440dabfe6ec4999902b70125eb106f44df54f
盛堡镇 <Administrator@sjk001-PC>
2016-04-20 14:39:21 +0800

    业务签收监听

---
+++ app-server/src/main/java/com/wangge/app/server/event/afterSalesmanSignforEvent.java
@@ -0,0 +1,68 @@
+package com.wangge.app.server.event;
+
+import org.springframework.context.ApplicationEvent;
+
+import com.alibaba.fastjson.JSONObject;
+/**
+ * 
+* @ClassName: afterSignforEvent
+* @Description: TODO(签收事件)
+* @author A18ccms a18ccms_gmail_com
+* @date 2016年4月16日 下午2:31:41
+*
+ */
+public class afterSalesmanSignforEvent extends ApplicationEvent {
+
+
+  /**
+  * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
+  */
+  
+  private static final long serialVersionUID = 1L;
+  private String userId;
+  private String coordinates;
+  private  int isPrimaryAccount;
+  private String  childId;
+  private int  type;
+  public afterSalesmanSignforEvent(String userId, String coordinates, int isPrimaryAccount, String childId, int type) {
+    super("");
+    this.userId = userId;
+    this.coordinates = coordinates;
+    this.isPrimaryAccount = isPrimaryAccount;
+    this.childId = childId;
+    this.type = type;
+  }
+  public String getUserId() {
+    return userId;
+  }
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+  public String getCoordinates() {
+    return coordinates;
+  }
+  public void setCoordinates(String coordinates) {
+    this.coordinates = coordinates;
+  }
+  public int getIsPrimaryAccount() {
+    return isPrimaryAccount;
+  }
+  public void setIsPrimaryAccount(int isPrimaryAccount) {
+    this.isPrimaryAccount = isPrimaryAccount;
+  }
+  public String getChildId() {
+    return childId;
+  }
+  public void setChildId(String childId) {
+    this.childId = childId;
+  }
+  public int getType() {
+    return type;
+  }
+  public void setType(int type) {
+    this.type = type;
+  }
+  
+  
+
+}
