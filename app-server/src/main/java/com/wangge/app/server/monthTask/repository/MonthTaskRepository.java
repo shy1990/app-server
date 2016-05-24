95043bcd8def5522e04a20021a23acf805f73212
yangqc <aqicaij@qq.com>
2016-05-24 15:00:15 +0800

    添加任务返回条件

--- app-server/src/main/java/com/wangge/app/server/monthTask/repository/MonthTaskRepository.java
+++ app-server/src/main/java/com/wangge/app/server/monthTask/repository/MonthTaskRepository.java
@@ -12,6 +12,6 @@ import com.wangge.app.server.monthTask.entity.MonthTask;
 
 public interface MonthTaskRepository extends JpaRepository<MonthTask, Long> {
 	
-	@Query(" select m  from MonthTask m where m.month=?1 and m.agentid=?2")
+	@Query(" select m  from MonthTask m where m.month=?1 and m.agentid=?2 and m.status=1 ")
 	MonthTask findFirstByMonth(String month,String agentid);
 }
