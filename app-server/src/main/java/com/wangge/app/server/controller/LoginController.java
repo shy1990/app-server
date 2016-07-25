ef139041bbba6d23e2ec3fe18f4a550bba568ecd
954691592@qq.com <954691592@qq.com>
2016-07-25 17:49:07 +0800

    提交本地

--- app-server/src/main/java/com/wangge/app/server/controller/LoginController.java
+++ app-server/src/main/java/com/wangge/app/server/controller/LoginController.java
@@ -1,32 +1,26 @@
 package com.wangge.app.server.controller;
 
 
-import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.alibaba.fastjson.JSONObject;
-import com.wangge.app.server.service.AssessService;
-import com.wangge.app.server.service.ChildAccountService;
-import com.wangge.app.server.service.SalaryService;
-import com.wangge.app.server.service.SalesmanService;
+import com.wangge.app.util.LogUtil;
+
 
 @RestController
 @RequestMapping(value = "/v1")
 public class LoginController {
 	
-	@Resource
-	private SalesmanService salesmanService;
-	@Resource
-	private AssessService assessService;
-	@Resource
-	private ChildAccountService childAccountService;
-	@Resource
-	private SalaryService salaryService;
+	 RestTemplate restTemplate = new RestTemplate();
+	  private String url = AppInterface.url + "monthTask/";
 	/**
 	 * 登录 
 	 * @param json
@@ -34,9 +28,38 @@ public class LoginController {
 	 */
 	@RequestMapping(value = "/login",method = RequestMethod.POST)
 
-  public String login(@RequestBody JSONObject jsons){
-    
-     return "";
+  public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, Object> talMap){
+		 return handleResult(sendRest("MonthTaskSubs", "post", talMap));
   }
+	
+	private ResponseEntity<Map<String, Object>> sendRest(String methodUrl, String restType, Map<String, Object> params)  {
+	    try {
+	      return RestTemplateUtil.sendRestForMap(restTemplate, restType, url + methodUrl, params);
+	    } catch (Exception e) {
+	      LogUtil.error("路径"+url + methodUrl+"---请求方法:"+restType, e);
+	      Map<String,Object> repMap=new HashMap<>();
+	      repMap.put("msg", "数据服务器异常");
+	      return new ResponseEntity<Map<String, Object>>(repMap, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	  }
+	
+	  /**
+	   * handleResult:处理返回结果中包含错误信息的情况. <br/>
+	   * 
+	   * @author yangqc
+	   * @param responseEntitu
+	   * @return
+	   * @since JDK 1.8
+	   */
+	  private ResponseEntity<Map<String, Object>> handleResult(ResponseEntity<Map<String, Object>> responseEntitu) {
+	    Map<String, Object> remap = responseEntitu.getBody();
+	    if (null != remap.get("error")) {
+	      if ("1".equals(remap.get("code"))) {
+	        return new ResponseEntity<Map<String, Object>>(remap, HttpStatus.INTERNAL_SERVER_ERROR);
+	      }
+	      return new ResponseEntity<Map<String, Object>>(remap, HttpStatus.NOT_FOUND);
+	    }
+	    return responseEntitu;
+	  }
 }
 
