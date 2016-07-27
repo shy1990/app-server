2d80f54f09fcac3c488455c7006a41c05f8d75fa
yangqc <aqicaij@qq.com>
2016-07-27 09:38:27 +0800

    简单重构已完成

--- app-server/src/main/java/com/wangge/app/server/CustomTaskController.java
+++ app-server/src/main/java/com/wangge/app/server/CustomTaskController.java
@@ -1,14 +1,9 @@
 package com.wangge.app.server;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,28 +13,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-
-import com.wangge.app.constant.AppInterface;
-import com.wangge.app.server.customTask.entity.CustomMessages;
-import com.wangge.app.server.customTask.entity.CustomTask;
-import com.wangge.app.server.customTask.server.CustomTaskServer;
-import com.wangge.app.server.customTask.server.ImplCustomTaskServe;
-import com.wangge.app.server.util.DateUtil;
-import com.wangge.app.util.LogUtil;
-import com.wangge.app.util.RestTemplateUtil;
 
 @Controller
 @RequestMapping(value = "/v1/customTask")
-public class CustomTaskController {
-  RestTemplate restTemplate = new RestTemplate();
-  private String url = AppInterface.url + "customTask/";
+public class CustomTaskController extends BaseController{
+  private static final String  controllerUrl ="customTask/";
   
   // 查询月任务详情
   @RequestMapping(value = "/{salesmanId}", method = RequestMethod.GET)
   public ResponseEntity<Map<String, Object>> findAllTask(HttpServletRequest request, @PathVariable String salesmanId,
       Pageable pageRequest) {
-    return handleResult(sendRest(salesmanId, "get", null));
+    return handleResult(sendRest(controllerUrl+salesmanId, "get", null));
   }
   
   /**
@@ -54,7 +38,7 @@ public class CustomTaskController {
   public String detail(@PathVariable("id") String customTaskId, @PathVariable("salesmanId") String salesmanId,
       HttpServletRequest request, Model model) {
     ResponseEntity<Map<String, Object>> rEntity = handleResult(
-        sendRest("detail/" + customTaskId + "/" + salesmanId, "get", null));
+        sendRest(controllerUrl+"detail/" + customTaskId + "/" + salesmanId, "get", null));
     if (!rEntity.getStatusCode().equals(HttpStatus.OK)) {
       return "远程服务器出错,请与管理员联系";
     }
@@ -70,46 +54,7 @@ public class CustomTaskController {
    */
   @RequestMapping(value = "/message", method = RequestMethod.POST)
   public ResponseEntity<Map<String, Object>> createSub(@RequestBody Map<String, Object> params) {
-    return handleResult(sendRest("message", "post", params));
+    return handleResult(sendRest(controllerUrl+"message", "post", params));
   }
-  
-  /**
-   * sendRest:发送RestTemplate的rest请求. <br/>
-   * 
-   * @author yangqc
-   * @param methodUrl
-   * @param restType
-   * @param params
-   * @return
-   * @since JDK 1.8
-   */
-  private ResponseEntity<Map<String, Object>> sendRest(String methodUrl, String restType, Map<String, Object> params) {
-    try {
-      return RestTemplateUtil.sendRestForMap(restTemplate, restType, url + methodUrl, params);
-    } catch (Exception e) {
-      LogUtil.error("路径" + url + methodUrl + "---请求方法:" + restType, e);
-      Map<String, Object> repMap = new HashMap<>();
-      repMap.put("msg", "数据服务器异常");
-      return new ResponseEntity<Map<String, Object>>(repMap, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-  
-  /**
-   * handleResult:处理返回结果中包含错误信息的情况. <br/>
-   * 
-   * @author yangqc
-   * @param responseEntitu
-   * @return
-   * @since JDK 1.8
-   */
-  private ResponseEntity<Map<String, Object>> handleResult(ResponseEntity<Map<String, Object>> responseEntitu) {
-    Map<String, Object> remap = responseEntitu.getBody();
-    if (null != remap.get("error")) {
-      if ("1".equals(remap.get("code"))) {
-        return new ResponseEntity<Map<String, Object>>(remap, HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-      return new ResponseEntity<Map<String, Object>>(remap, HttpStatus.NOT_FOUND);
-    }
-    return responseEntitu;
-  }
+ 
 }
---
+++ app-server/src/main/java/com/wangge/app/server/OrderCountController.java
@@ -0,0 +1,25 @@
+package com.wangge.app.server;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
+
+@RestController
+@RequestMapping("/v1/orderCount")
+public class OrderCountController extends BaseController {
+  String controllerUrl = "/orderCount/";
+  
+  @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+  public ResponseEntity<Map<String, Object>> getOderCount(@PathVariable String userId, HttpServletRequest request) {
+    Map<String, Object> params = WebUtils.getParametersStartingWith(request, "");
+    return handleResult(sendRest(controllerUrl + userId, "get", params));
+  }
+  
+}
