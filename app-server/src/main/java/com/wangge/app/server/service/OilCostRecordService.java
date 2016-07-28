e84fff3c81c5bff797440d09fe2fda9674ac502f
yangqc <aqicaij@qq.com>
2016-07-28 09:44:19 +0800

    修改dateUtil类的引用

--- app-server/src/main/java/com/wangge/app/server/service/OilCostRecordService.java
+++ app-server/src/main/java/com/wangge/app/server/service/OilCostRecordService.java
@@ -31,9 +31,9 @@ import com.wangge.app.server.repository.ChildAccountRepostory;
 import com.wangge.app.server.repository.OilCostRecordRepository;
 import com.wangge.app.server.repositoryimpl.OilRecordImpl;
 import com.wangge.app.server.util.ChainageUtil;
-import com.wangge.app.server.util.DateUtil;
 import com.wangge.app.server.util.JWtoAdrssUtil;
 import com.wangge.app.server.vo.OilCostRecordVo;
+import com.wangge.app.util.DateUtil;
 
 @Service
 public class OilCostRecordService {
