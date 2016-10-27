8bac91e5b28cade1a2b305708a3f8ee2a91223e3
yangqc <aqicaij@qq.com>
2016-10-27 15:58:14 +0800

    修改提交bug

--- app-server/src/main/java/com/wangge/app/server/entity/OrderSignfor.java
+++ app-server/src/main/java/com/wangge/app/server/entity/OrderSignfor.java
@@ -7,11 +7,12 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
